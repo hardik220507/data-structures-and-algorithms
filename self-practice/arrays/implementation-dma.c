31bf0fc7f67ac18b20f1d673435cbebb9de4c38c
hardik <hardik220507@gmail.com>
2026-09-08 10:30:40 +0530

    Array Traversal

--- self-practice/arrays/implementation-dma.c
+++ self-practice/arrays/implementation-dma.c
@@ -10,7 +10,7 @@ int main(){
     int n;
     printf("Enter the number of elements:");
     scanf("%d",&n);
-    int *arr = (int*)malloc(sizeof(*arr) * n);
+    int *arr = (int*)malloc(sizeof(int) * n);
     printf("Enter the elements:");
     for(int i=0;i<n;i++){
         scanf("%d",arr+i);
